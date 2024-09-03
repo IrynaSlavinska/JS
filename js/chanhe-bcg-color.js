c940e7ec25147eb5b2a83dcfbc06778469f88f53
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-09-03 20:43:36 +0300

    change bcg color

---
+++ js/chanhe-bcg-color.js
@@ -0,0 +1,17 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+// console.log(document.body.style.backgroundColor);
+console.log(window.getComputedStyle(document.body).backgroundColor);
+
+button.addEventListener("click", () => {
+  span.textContent = getRandomHexColor();
+
+  document.body.style.backgroundColor = span.textContent;
+});
