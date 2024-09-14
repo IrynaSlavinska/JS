56103b013c644bd244f2d617b606d8bfce80448f
Iryna Slavinska <lanfirtiger.is@gmail.com>
2024-09-14 22:58:18 +0300

    prepared markup

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,14 +1,31 @@
 const spoilerTitles = document.querySelectorAll(".spoiler-title");
 
 spoilerTitles.forEach((item) => {
-  item.addEventListener("click", function () {
-    const text = this.nextElementSibling;
+  item.addEventListener("click", (event) => {
+    const clickedElement = event.currentTarget;
+    const text = clickedElement.nextElementSibling;
+
     text.style.display = text.style.display === "block" ? "none" : "block";
 
-    if (this.classList.contains("open")) {
-      this.classList.remove("open");
+    if (clickedElement.classList.contains("open")) {
+      clickedElement.classList.remove("open");
     } else {
-      this.classList.add("open");
+      clickedElement.classList.add("open");
     }
   });
 });
+
+// const spoilerTitles = document.querySelectorAll(".spoiler-title");
+
+// spoilerTitles.forEach((item) => {
+//   item.addEventListener("click", function () {
+//     const text = this.nextElementSibling;
+//     text.style.display = text.style.display === "block" ? "none" : "block";
+
+//     if (this.classList.contains("open")) {
+//       this.classList.remove("open");
+//     } else {
+//       this.classList.add("open");
+//     }
+//   });
+// });
